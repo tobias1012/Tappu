180a06b30ddc7f8615eed1c317ef3126151389aa
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2025-02-10 16:34:32 +0100

    add pointer command and quit command

--- src/main/scala/tappu/util/Assembler.scala
+++ src/main/scala/tappu/util/Assembler.scala
@@ -8,21 +8,24 @@ import scala.util.parsing.combinator._
 
 class AssemblerParser extends RegexParsers {
   def comment: Parser[String] = """//.*""".r ^^ { _.toString }
-  def instruction: Parser[Int] = ">" ^^ { _=> 0 } | "<" ^^ { _ => 1 } | "+" ^^ { _ => 2 } | "-" ^^ { _ => 3 } | "." ^^ { _ => 4 } | "," ^^ { _ => 5 } | "[" ^^ { _ => 6 } | "]" ^^ { _ => 7 }
-  //def label: Parser[String] = """[A-z].+?:""".r ^^ { _.toString.substring(0, _.toString.length - 1) }
-
+  def instruction: Parser[Int] =  ">" ^^ { _=> 0 } 
+                                | "<" ^^ { _ => 1 } 
+                                | "+" ^^ { _ => 2 } 
+                                | "-" ^^ { _ => 3 } 
+                                | "." ^^ { _ => 4 } 
+                                | "," ^^ { _ => 5 } 
+                                | "[" ^^ { _ => 6 } 
+                                | "]" ^^ { _ => 7 } 
+                                | "p" ^^ { _ => 8}
+                                | "q" ^^ { _ => 9}
   def program: Parser[List[Int]] = (comment ~ program ^^ { case c ~ p => p }
                                    | rep(instruction) ^^ {case i => i }
                                    )
 }
 
 object Assembler extends  AssemblerParser{
-  val labels = collection.mutable.Map[String, Int]()
-
   def asm(asm: String): Seq[Int] = {
-    val source = Source.fromFile(asm)
-    var pc = 0
-    
+    val source = Source.fromFile(asm)    
     
     val content = source.mkString
 
